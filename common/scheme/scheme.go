7cffe26defbb93789c06be5f5b4b2ed5677c76fc
Emmanuel <emmanuelm41@gmail.com>
2021-10-12 11:54:13 -0300

    fix typo on scheme ids

--- common/scheme/scheme.go
+++ common/scheme/scheme.go
@@ -1,13 +1,13 @@
 package scheme
 
-const DefaultSchemeID = "pedersen-bls-chanined"
+const DefaultSchemeID = "pedersen-bls-chained"
 
 type Scheme struct {
 	ID              string
 	DecouplePrevSig bool
 }
 
-var schemes = []Scheme{{ID: "pedersen-bls-chanined", DecouplePrevSig: false}, {ID: "pedersen-bls-unchanined", DecouplePrevSig: true}}
+var schemes = []Scheme{{ID: "pedersen-bls-chained", DecouplePrevSig: false}, {ID: "pedersen-bls-unchained", DecouplePrevSig: true}}
 
 func GetSchemeByID(id string) (scheme Scheme, found bool) {
 	for _, t := range schemes {
